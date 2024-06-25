e1983aa48eebf70d9b9938b3b7177ce10231788a
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-06-25 16:31:54 +0500

    updation done in slider

--- project1/src/javascript.js
+++ project1/src/javascript.js
@@ -2,31 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     
 
-let imageCard=document.querySelector(".reviewer-img");
-const reviewersnodeList=document.querySelectorAll(".float-image");
 const dotsTrack=document.querySelector("#slider-buttons");
 const dots=Array.from(dotsTrack.children);
 let activeDot=dots[0];
 const carousel=document.querySelector(".slider-carousel");
 const slideCards=Array.from(carousel.querySelectorAll(".card"));
-let card=document.querySelector(".card");
-let slideWidth=card.getBoundingClientRect().width;
 
-console.log(slideWidth);
-
-// slideCards[0].style.left=slideWidth * 0 + 'px';
-// slideCards[1].style.left=slideWidth  + 'px';
-// slideCards[2].style.left=slideWidth * 2 + 'px';
-// slideCards[3].style.left=slideWidth * 3 + 'px';
-// slideCards[4].style.left=slideWidth * 4 + 'px';
+// let slideWidth=card.getBoundingClientRect().width;
 
 
-// slideCards.forEach((slide,index) => {
-
-//     slide.style.left=slideWidth * (index+1) + 'px';
-
-
-// });
 
 
 const movetoSlide =(track,currentSlide,targetSlide,leftValue)=>{
@@ -52,24 +36,10 @@ const currentSlide=carousel.querySelector(".current-card");
 const currentdot=dotsTrack.querySelector(".active");
 const dotIndex = dots.findIndex(dot => dot===targetDot);
 const targetSlide=slideCards[dotIndex];
-
-
-console.log(currentSlide);
-console.log(currentdot);
-console.log(dotIndex);
-console.log(targetSlide);
-
 const computedStyle = window.getComputedStyle(targetSlide);
 const leftValue = computedStyle.getPropertyValue('left');
-console.log(`Left position value: ${leftValue}`);
-console.log(carousel);
-
-
 movetoSlide(carousel,currentSlide,targetSlide,leftValue);
-
-
-
-})
+});
 
 
 
@@ -80,15 +50,18 @@ for(let circle of dots){
      activeDot.classList.remove("active");
      activeDot=circle;
      activeDot.classList.add("active");
-
-
-
     });
 }
 
 
 
+document.querySelector('input[type="submit"]').addEventListener("click", function(event){
+  event.preventDefault();
+ 
 
+
+
+});
 
 
 
