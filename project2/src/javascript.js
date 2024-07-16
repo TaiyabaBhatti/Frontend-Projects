2c830e76d387a6c3cf36055f6d5f95b9939e778b
TaiyabaBhatti <taiyababhattii@gmail.com>
2024-07-16 21:43:02 +0500

    added

--- project2/src/javascript.js
+++ project2/src/javascript.js
@@ -4,7 +4,6 @@ let childLike;
 
 
 function likeActive(car){
-
     if(isDoubleClick){
         childLike.classList.add("liked");
         childLike.classList.remove("fa-regular");
@@ -28,6 +27,63 @@ likeActive(childLike);
 
 
 
+let screenWidth=window.innerWidth;
+
+const carCategory =document.querySelector(".car-category");
+const categoryBlock =document.querySelector(".category-block");
+const dropDown =  carCategory.querySelector(".fa-caret-down")
+
+const dropUp =  carCategory.querySelector(".fa-caret-up")
+
+let statusDefault=true;
+
+function checkingWidth(estimatedWidth){
+    if(estimatedWidth <= 572){
+        categoryBlock.style.display="none";
+        dropDown.style.display="block";
+        }
+        
+        else if(estimatedWidth > 572){
+            categoryBlock.style.display="flex";
+            dropDown.style.display="none";
+            dropUp.style.display="none";
+           
+        }
+}
+
+window.addEventListener("resize",()=>{
+    screenWidth=window.innerWidth;
+checkingWidth(screenWidth);
+});
+checkingWidth(screenWidth);
+
+function settingArrows(){
+
+    if(statusDefault){
+        dropDown.style.display="none";
+        dropUp.style.display="block";
+        statusDefault=false;
+    }
+
+    else{
+        dropDown.style.display="block";
+        dropUp.style.display="none";
+        statusDefault=true;
+    }
+
+}
+
+dropDown.addEventListener("click",()=>{
+    categoryBlock.style.display="flex";
+  settingArrows();
+  
+});
+
+dropUp.addEventListener("click",()=>{
+    categoryBlock.style.display="none";
+    settingArrows();
+    
+});
 
 
 
